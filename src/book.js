a86223723452e64b7a62ac99be9c3782412c4cee
Jake <jsmith181997@gmail.com>
2024-05-05 09:55:05 -0400

    Add eslint and prettier to project

--- src/book.js
+++ src/book.js
@@ -1,33 +1,30 @@
-
-import { myLibrary } from "./library";
+import { myLibrary, refreshCards } from './library';
 
 export class Book {
-    constructor(title, author, pages, hasRead) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.hasRead = hasRead;
-        this.bookID = this.assignID();
+  constructor(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+    this.bookID = this.assignID();
+  }
+
+  readBook() {
+    if (this.hasRead) {
+      return `You've already read ${this.title} ID: ${this.bookID}`;
     }
 
-    readBook() {
+    this.hasRead = true;
+    refreshCards();
+    return `You've finished ${this.title} ID: ${this.bookID}`;
+  }
 
-        if (this.hasRead) {
-            return `You've already read ${this.title} ID: ${this.bookID}`;
-        }
+  /** If book doesn't have an id, assign one. */
+  assignID() {
+    return myLibrary.length;
+  }
 
-        this.hasRead = true;
-        refreshCards();
-        return `You've finished ${this.title} ID: ${this.bookID}`;
-    }
-
-    /** If book doesn't have an id, assign one. */
-    assignID() {
-        return myLibrary.length;
-
-    }
-
-    toggleComplete() {
-        this.hasRead = !this.hasRead;
-    }
+  toggleComplete() {
+    this.hasRead = !this.hasRead;
+  }
 }
--- src/library.js
+++ src/library.js
@@ -1,113 +1,109 @@
-import { bookTileBuilder } from "./book-builder";
-import "../style.css";
+import { bookTileBuilder } from './book-builder';
+import { Book } from './book';
+import '../style.css';
 
-const addBookButton = document.querySelector(".addBook");
-const newBookPopup = document.querySelector(".bookPopup");
-const submitButton = document.getElementById("submit");
-const cancelButton = document.getElementById("cancel");
+const addBookButton = document.querySelector('.addBook');
+const newBookPopup = document.querySelector('.bookPopup');
+const submitButton = document.getElementById('submit');
+const cancelButton = document.getElementById('cancel');
 
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const pageCountInput = document.getElementById("pages");
-const finishedBookInput = document.getElementById("read");
-const bookshelf = document.getElementById("bookshelf");
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pageCountInput = document.getElementById('pages');
+const finishedBookInput = document.getElementById('read');
+const bookshelf = document.getElementById('bookshelf');
 
 export const myLibrary = [];
 
-
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
     resetLibrary();
-}, { once: true });
+  },
+  { once: true }
+);
 
-document.addEventListener("DOMContentLoaded", () => {
-    dummyBooks();
-    refreshCards();
+document.addEventListener('DOMContentLoaded', () => {
+  dummyBooks();
+  refreshCards();
 });
 
-
-addBookButton.addEventListener("click", () => {
-    newBookPopup.showModal();
+addBookButton.addEventListener('click', () => {
+  newBookPopup.showModal();
 });
 
-cancelButton.addEventListener("click", () => {
-    clearForm();
-    newBookPopup.close();
-})
+cancelButton.addEventListener('click', () => {
+  clearForm();
+  newBookPopup.close();
+});
 
-submitButton.addEventListener("click", (e) => {
-    e.preventDefault(); // avoid submitting form
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault(); // avoid submitting form
 
-    generateBook();
-    newBookPopup.close();
-    clearForm();
+  generateBook();
+  newBookPopup.close();
+  clearForm();
 });
 
 function clearForm() {
-    titleInput.value = "";
-    authorInput.value = "";
-    pageCountInput.value = 1;
-    finishedBookInput.checked = false;
+  titleInput.value = '';
+  authorInput.value = '';
+  pageCountInput.value = 1;
+  finishedBookInput.checked = false;
 }
 
 // Create a new book and add it too the library.
 function generateBook() {
+  let read = finishedBookInput.checked ? true : false;
+  let newBook = new Book(
+    titleInput.value,
+    authorInput.value,
+    pageCountInput.value,
+    read
+  );
 
-    let read = (finishedBookInput.checked) ? true : false;
-    let newBook = new Book(titleInput.value, authorInput.value, pageCountInput.value, read);
-
-    addBookToLibrary(newBook);
-    refreshCards();
+  addBookToLibrary(newBook);
+  refreshCards();
 }
 
 function addBookToLibrary(newBook) {
-    newBook.assignID();
-    myLibrary.push(newBook);
-    console.log(`${newBook.title} added to the library! ID: ${newBook.bookID}`);
-    return `${newBook.title} added to the library! ID: ${newBook.bookID}`;
+  newBook.assignID();
+  myLibrary.push(newBook);
+  console.log(`${newBook.title} added to the library! ID: ${newBook.bookID}`);
+  return `${newBook.title} added to the library! ID: ${newBook.bookID}`;
 }
 
-function refreshCards() {
+export function refreshCards() {
+  clearCards();
 
-    clearCards();
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        const book = myLibrary[i];
-        book.bookID = i;
-        bookTileBuilder(book, bookshelf);
-    }
-
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    book.bookID = i;
+    bookTileBuilder(book, bookshelf);
+  }
 }
 
 function resetLibrary() {
-    myLibrary.splice(0, myLibrary.length)
-    refreshCards();
+  myLibrary.splice(0, myLibrary.length);
+  refreshCards();
 }
 
 function clearCards() {
-    bookshelf.innerHTML = "";
-
+  bookshelf.innerHTML = '';
 }
 
 export function removeCard(cardID) {
-    myLibrary.splice(cardID, 1);
-    refreshCards();
+  myLibrary.splice(cardID, 1);
+  refreshCards();
 }
-
-
 
 function dummyBooks() {
-    // Add some sample books to start
-    myLibrary.push(new Book('The Poppy War', 'RF Kuang', 452, true));
-    myLibrary.push(new Book('The Dragon Republic', 'RF Kuang', 618, false, 1));
-    myLibrary.push(new Book('The Burning God', 'RF Kuang', 705, false, 2));
-
-
+  // Add some sample books to start
+  myLibrary.push(new Book('The Poppy War', 'RF Kuang', 452, true));
+  myLibrary.push(new Book('The Dragon Republic', 'RF Kuang', 618, false, 1));
+  myLibrary.push(new Book('The Burning God', 'RF Kuang', 705, false, 2));
 }
 
-
 myLibrary.forEach((book) => {
-    console.log(book.readBook());
-})
-
-
-
+  console.log(book.readBook());
+});
