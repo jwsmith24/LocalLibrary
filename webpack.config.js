8e3704b04b3ed1339c4113da4467dc571cea884a
Jake <jsmith181997@gmail.com>
2024-05-05 17:07:57 -0400

    Update webpack config output to docs for GH pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     index: './src/index.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name].bundle.js',
   },
   devtool: 'source-map',
